12390ee43d18f797a5902fe4c6e4f5ec37cc53ee
Adrian Jaramillo <adrianjagudelo@gmail.com>
2024-03-19 17:43:12 -0500

    se crearon aarays y json

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,31 @@
 import "./Home.css"
+import { inventario } from "../utils/medicamentos";
 
 export function Home(){
-    return(
+
+//zona de manipulacion de datos
+    let nombrePaciente = "adrian jaramillo";
+    let sedePaciente = "Suramericana";
+    let nombreEspecialista = "Jorge Gonzalez"
+    console.log(inventario)
+
+    // a los arreglos los puedo manipular para controlar que informacion voy a presentar
+
+    //1.funcion de filtrado de datos
+    //condicion de filtrado(pregunta (si/no))
+    
+    let resultado = inventario.filter(function(auxiliar){
+        return(auxiliar.nombre == "acetaminofen") //condicion de filtrado
+    })
+    console.log(resultado)
+
+    return(  //zona de renderizado
         <>
             <section className="container mt-5">
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <h3 className="fuente">Servicios a un clic</h3>
-                        <h2 className="text-muted">Hola, <span className="fuente">ADRIAN</span></h2>
+                        <h2 className="text-muted">Hola, <span className="fuente">{nombrePaciente}</span></h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima nostrum amet dolore. Assumenda quos reprehenderit quis illum sed iste quibusdam ducimus ad soluta. Optio, dolores. Dolor tempora beatae perferendis animi eum deserunt molestiae a alias, reiciendis mollitia provident fugit voluptatum! Labore eveniet vitae sed dolorum doloribus saepe id officiis voluptates?</p>
                     </div>
                     <div className="col-12 col-md-6">
@@ -23,7 +41,8 @@ export function Home(){
                             <i className="bi bi-person-badge fs-1 fuente"></i>
                             </div>
                             <div className="col-12 col-md-10">
-                                <p className="mt-4">MEDICO DE FAMILIA</p>
+                                <p className="mt-4 mb-1">MEDICO DE FAMILIA</p>
+                                <h6>{nombreEspecialista}</h6>
                             </div>
                        </div>
                        <div className="row">
@@ -31,7 +50,9 @@ export function Home(){
                                 <i className="bi bi-person-circle fs-1 fuente"></i>
                             </div>
                             <div className="col-12 col-md-10">
-                                <p className="mt-4">TIPO DE AFILIADO</p>
+                                <p className="mt-4 mb-1">TIPO DE AFILIADO</p>
+                                <h6 className="mb-1" > COTIZANTE</h6>
+                                <h6 className="mb-1">GRUPO DE INGRESOS: B</h6>
                             </div>
                        </div>
                        <div className="row">
---
+++ src/utils/especialistas.js
@@ -0,0 +1,23 @@
+let especilistaUno = {
+    nombre: "Alvaro Angel",
+    especialidad: "Ortopedia",
+    registro: "22230",
+    salario: 5000000,
+    jornada: "completa",
+    sede: "Sao paulo"
+}
+
+let especilistaDos = {
+    nombre: "Felipe Betancur",
+    especialidad: "Internista",
+    registro: "12345",
+    salario: 5000000,
+    jornada: "media",
+    sede: "Industriales"
+}
+
+let especialistas = [
+    especilistaUno,
+    especilistaDos
+]
+
